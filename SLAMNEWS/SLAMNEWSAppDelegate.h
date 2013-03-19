c401f5111c7dba55d97fd7efc2b9a34ca9d7bbcf
Abigail Diaz <abigail_diaz2002@yahoo.com>
2013-03-19 09:37:24 -0400

    Initial Commit

---
+++ SLAMNEWS/SLAMNEWSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAMNEWSAppDelegate.h
+//  SLAMNEWS
+//
+//  Created by Abigail Diaz on 3/19/13.
+//  Copyright (c) 2013 Abigail Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAMNEWSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
